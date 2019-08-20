7c9bbc28899cf99fc8d18d74cbfb1e9c3c151785
kerghan <kerghan.the.terrible@yandex.ru>
2019-08-21 00:46:32 +0300

    ScheduledExecutorServiceTest

---
+++ src/test/java/test/concurrency/ScheduledExecutorServiceTest.java
@@ -0,0 +1,33 @@
+package test.concurrency;
+
+import org.junit.Test;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+public class ScheduledExecutorServiceTest {
+
+
+    private int counter = 0;
+
+    @Test
+    public void overlapExecutionTest() throws InterruptedException {
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        service.scheduleWithFixedDelay(() -> {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            counter++;
+            System.out.println("launch number: " + counter);
+        }, 0, 5, TimeUnit.SECONDS);
+            Thread.sleep(30000);
+        service.shutdown();
+        assertEquals(2, counter);
+    }
+
+}
